34b45413ac232db7abfe02315bddb3a8e4afdd41
nawal alatwi <47890182+bitonal@users.noreply.github.com>
2022-10-29 09:36:10 +0300

    add c sharp file retrive data of students

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ass1
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            // Read files 
+            FileStream stream = null;
+            string fileName = "C:/Users/Dell/myProjectNawal/DataStudent.txt";
+            try
+            {
+                stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                StreamReader r = new StreamReader(stream);
+                string text = r.ReadToEnd();
+                Console.WriteLine(text);
+                r.Close();
+                stream.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
